c65f0a1dede2ab01cfbb006e8eaa9d3d37e3a521
Dennis-van-Gils <vangils.dennis@gmail.com>
2020-05-14 15:55:53 +0200

    Added tests

--- tests/test_DvG_debug_functions.py
+++ tests/test_DvG_debug_functions.py
@@ -32,9 +32,42 @@ def test_pft():
 
 
 
+def test_pft_overshoot_callstack():
+    try:
+        0/0
+    except ZeroDivisionError as err:
+        with mock.patch('sys.stdout', new=io.StringIO()) as fake_stdout:
+            pft(err, 30)
+            
+        assert fake_stdout.getvalue().split('\n')[-2] == \
+            '\x1b[1;31mZeroDivisionError: \x1b[1;37mdivision by zero'
+
+
+
 def test_pft_err_as_string():
     with mock.patch('sys.stdout', new=io.StringIO()) as fake_stdout:
         pft("Custom error string", 1)
         
     assert fake_stdout.getvalue().split('\n')[-2] == \
         '\x1b[1;31mError: \x1b[1;37mCustom error string'
+        
+def test_pft():
+    try:
+        0/0
+    except ZeroDivisionError as err:
+        with mock.patch('sys.stdout', new=io.StringIO()) as fake_stdout:
+            pft(err, 1)
+
+
+
+def test_pft_descr_abbr():
+    try:
+        0/0
+    except ZeroDivisionError as err:
+        err.abbreviation = 'abbr'
+        err.description = 'descr'
+        with mock.patch('sys.stdout', new=io.StringIO()) as fake_stdout:
+            pft(err, 1)
+            
+        assert fake_stdout.getvalue().split('\n')[-2] == \
+            '\x1b[1;31mZeroDivisionError: \x1b[1;37mabbr: descr'
