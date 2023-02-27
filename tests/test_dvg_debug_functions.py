6a11ac449cf116d8e45d866a2c27bce00919db4d
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2023-02-27 20:04:49 +0100

    Added coverage results

--- tests/test_dvg_debug_functions.py
+++ tests/test_dvg_debug_functions.py
@@ -1,75 +1,69 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import io
+import unittest
+from unittest import mock
+
 from dvg_debug_functions import (
     dprint,
     tprint,
     ANSI,
     print_fancy_traceback as pft,
 )
-from unittest import mock
-import io
 
 
-def test_dprint():
-    with mock.patch("sys.stdout", new=io.StringIO()) as fake_stdout:
-        dprint("No color")
-
-    assert fake_stdout.getvalue() == "No color\n"
-
-
-def test_dprint_in_red():
-    with mock.patch("sys.stdout", new=io.StringIO()) as fake_stdout:
-        dprint("In red", ANSI.RED)
-
-    assert fake_stdout.getvalue() == "\x1b[1;31mIn red\x1b[1;37m\n"
-
-
-def test_tprint():
-    with mock.patch("sys.stdout", new=io.StringIO()) as fake_stdout:
-        tprint("No color")
-
-    assert fake_stdout.getvalue()[-9:] == "No color\n"
-
-
-def test_pft():
-    try:
-        0 / 0
-    except ZeroDivisionError as err:
+class TestAll(unittest.TestCase):
+    def test_dprint(self):
         with mock.patch("sys.stdout", new=io.StringIO()) as fake_stdout:
-            pft(err, 1)
+            dprint("No color")
+
+        assert fake_stdout.getvalue() == "No color\n"
+
+    def test_dprint_in_red(self):
+        with mock.patch("sys.stdout", new=io.StringIO()) as fake_stdout:
+            dprint("In red", ANSI.RED)
+
+        assert fake_stdout.getvalue() == "\x1b[1;31mIn red\x1b[1;37m\n"
+
+    def test_tprint(self):
+        with mock.patch("sys.stdout", new=io.StringIO()) as fake_stdout:
+            tprint("No color")
+
+        assert fake_stdout.getvalue()[-9:] == "No color\n"
+
+    def test_pft(self):
+        try:
+            0 / 0
+        except ZeroDivisionError as err:
+            with mock.patch("sys.stdout", new=io.StringIO()) as fake_stdout:
+                pft(err, 1)
+
+            assert (
+                fake_stdout.getvalue().split("\n")[-2]
+                == "\x1b[1;31mZeroDivisionError: \x1b[1;37mdivision by zero"
+            )
+
+    def test_pft_overshoot_callstack(self):
+        try:
+            0 / 0
+        except ZeroDivisionError as err:
+            with mock.patch("sys.stdout", new=io.StringIO()) as fake_stdout:
+                pft(err, 50)
+
+            assert (
+                fake_stdout.getvalue().split("\n")[-2]
+                == "\x1b[1;31mZeroDivisionError: \x1b[1;37mdivision by zero"
+            )
+
+    def test_pft_err_as_string(self):
+        with mock.patch("sys.stdout", new=io.StringIO()) as fake_stdout:
+            pft("Custom error string", 1)
 
         assert (
             fake_stdout.getvalue().split("\n")[-2]
-            == "\x1b[1;31mZeroDivisionError: \x1b[1;37mdivision by zero"
+            == "\x1b[1;31mError: \x1b[1;37mCustom error string"
         )
 
 
-def test_pft_overshoot_callstack():
-    try:
-        0 / 0
-    except ZeroDivisionError as err:
-        with mock.patch("sys.stdout", new=io.StringIO()) as fake_stdout:
-            pft(err, 50)
-
-        assert (
-            fake_stdout.getvalue().split("\n")[-2]
-            == "\x1b[1;31mZeroDivisionError: \x1b[1;37mdivision by zero"
-        )
-
-
-def test_pft_err_as_string():
-    with mock.patch("sys.stdout", new=io.StringIO()) as fake_stdout:
-        pft("Custom error string", 1)
-
-    assert (
-        fake_stdout.getvalue().split("\n")[-2]
-        == "\x1b[1;31mError: \x1b[1;37mCustom error string"
-    )
-
-
-def test_pft():
-    try:
-        0 / 0
-    except ZeroDivisionError as err:
-        with mock.patch("sys.stdout", new=io.StringIO()) as fake_stdout:
-            pft(err, 1)
+if __name__ == "__main__":
+    unittest.main()
